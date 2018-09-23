1f4a22cb2060618b3e27e29548050ce450462748
Deri putra <deriputra771@gmail.com>
2018-09-23 20:01:38 +0700

    code

---
+++ deri/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace deri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.operator1 = new System.Windows.Forms.ComboBox();
+            this.angka1 = new System.Windows.Forms.TextBox();
+            this.angka2 = new System.Windows.Forms.TextBox();
+            this.hasilteks = new System.Windows.Forms.TextBox();
+            this.hitung = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // operator1
+            // 
+            this.operator1.FormattingEnabled = true;
+            this.operator1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "/",
+            "x"});
+            this.operator1.Location = new System.Drawing.Point(57, 12);
+            this.operator1.Name = "operator1";
+            this.operator1.Size = new System.Drawing.Size(121, 21);
+            this.operator1.TabIndex = 0;
+            // 
+            // angka1
+            // 
+            this.angka1.Location = new System.Drawing.Point(57, 49);
+            this.angka1.Name = "angka1";
+            this.angka1.Size = new System.Drawing.Size(100, 20);
+            this.angka1.TabIndex = 1;
+            this.angka1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // angka2
+            // 
+            this.angka2.Location = new System.Drawing.Point(57, 84);
+            this.angka2.Name = "angka2";
+            this.angka2.Size = new System.Drawing.Size(100, 20);
+            this.angka2.TabIndex = 2;
+            // 
+            // hasilteks
+            // 
+            this.hasilteks.Location = new System.Drawing.Point(57, 121);
+            this.hasilteks.Name = "hasilteks";
+            this.hasilteks.Size = new System.Drawing.Size(100, 20);
+            this.hasilteks.TabIndex = 3;
+            // 
+            // hitung
+            // 
+            this.hitung.Location = new System.Drawing.Point(57, 158);
+            this.hitung.Name = "hitung";
+            this.hitung.Size = new System.Drawing.Size(75, 23);
+            this.hitung.TabIndex = 4;
+            this.hitung.Text = "Hitung";
+            this.hitung.UseVisualStyleBackColor = true;
+            this.hitung.Click += new System.EventHandler(this.hasilteks_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Operator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nilai A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nilai B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Hasil";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 207);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hitung);
+            this.Controls.Add(this.hasilteks);
+            this.Controls.Add(this.angka2);
+            this.Controls.Add(this.angka1);
+            this.Controls.Add(this.operator1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox operator1;
+        private System.Windows.Forms.TextBox angka1;
+        private System.Windows.Forms.TextBox angka2;
+        private System.Windows.Forms.TextBox hasilteks;
+        private System.Windows.Forms.Button hitung;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
